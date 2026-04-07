23b34c4046b4bd1ceace62fd9001930a8714f0f7
isushh <160877840+isushh@users.noreply.github.com>
2026-04-07 05:33:19 +0530

    Update AuthContext.js

--- frontend/src/contexts/AuthContext.js
+++ frontend/src/contexts/AuthContext.js
@@ -96,22 +96,8 @@ const googleCallback = async (credential, role) => {
     setUser(userData);
   };
 
-const deleteInternship = async (id) => {
-    const token = localStorage.getItem("token");
-    return await axios.delete(`${API}/internships/${id}`, {
-        headers: { Authorization: `Bearer ${token}` }
-    });
-};
-
-const deleteApplication = async (id) => {
-    const token = localStorage.getItem("token");
-    return await axios.delete(`${API}/applications/${id}`, {
-        headers: { Authorization: `Bearer ${token}` }
-    });
-};
-
   return (
-    <AuthContext.Provider value={{ user, loading, token, login, register,user, deleteInternship, deleteApplication, adminRegister, googleCallback, logout, updateUser, axiosAuth }}>
+    <AuthContext.Provider value={{ user, loading, token, login, register, user, adminRegister, googleCallback, logout, updateUser, axiosAuth }}>
       {children}
     </AuthContext.Provider>
   );
